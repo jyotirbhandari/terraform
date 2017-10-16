d4f879d7695ff0a39f4855ae42fdd7b4d7f276d5
Jyotir Bhandari <jyotir@stuxbot.com>
2017-10-17 03:44:17 +0545

    Uploading code for elasticbenastalk with some changes in hash

--- aws/env/dev/elasticbeanstalk.tf
+++ aws/env/dev/elasticbeanstalk.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_s3_bucket" "beanstalk" {
-  bucket = "Q8DC0JWUGQUZMD5CINVEJTUQZDMZSDRU"
+  bucket = "12Q8DC0JWUGQUZMD5CINVEJTUQZDMZSDRU"
 }
 
 resource "aws_s3_bucket_object" "beanstalk" {
@@ -17,7 +17,7 @@ module "elasticbeanstalk" {
   env = "dev"
   eb_app_name = "eb-app"
   eb_app_version_name = "eb-app-version"
-  eb_env_name = "Q8DC0JWUGQUZMD5CINVEJTUQZDMZSDRU"
+  eb_env_name = "12Q8DC0JWUGQUZMD5CINVEJTUQZDMZSDRU"
   s3_bucket_id = "${aws_s3_bucket.beanstalk.id}"
   s3_bucket_object_id = "${aws_s3_bucket_object.beanstalk.id}"
   eb_solution_stack_name = "64bit Amazon Linux 2017.03 v2.5.0 running PHP 7.1"
