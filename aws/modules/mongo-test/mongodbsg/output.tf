52e72e105ff2f75b4d4dc73507b0946659c75222
Jyotir Bhandari <jyotir@stuxbot.com>
2018-01-12 12:46:23 +0545

    remove test directory

--- aws/modules/mongo-test/mongodbsg/output.tf
+++
@@ -1,4 +0,0 @@
-// Output value for the nat instance security group
-output "vpc_mongodbsg" {
-  value = "${aws_security_group.mongodbsg.id}"
-}
--- aws/modules/mongo-test/natsg/output.tf
+++
@@ -1,4 +0,0 @@
-// Output value for the nat instance security group
-output "vpc_natsg" {
-  value = "${aws_security_group.natsg.id}"
-}
